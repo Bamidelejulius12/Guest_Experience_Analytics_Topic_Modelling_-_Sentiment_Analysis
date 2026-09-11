3ca3ae6e55720a98a301af62a20e9800eb81fa9f
Bamidelejulius12 <babatundejulius911@gmail.com>
2026-09-11 02:05:08 +0100

    application completed and ready to be deployed to aws

--- src/utility/mlflow_setup.py
+++ src/utility/mlflow_setup.py
@@ -5,32 +5,34 @@ from dotenv import load_dotenv
 load_dotenv(override = True)
 from config.constant import DAGSHUB_REPO, DAGSHUB_USERNAME, experiment_name
 
-def setup_mlflow_connection():
-  
-    dagshub.init(
-        repo_owner=DAGSHUB_USERNAME,
-        repo_name=DAGSHUB_REPO,
-        mlflow=True
-    )
 
-    mlflow.set_experiment(experiment_name)
+# Local accessibilty or connection to mlflow
+# def setup_mlflow_connection():
+  
+#     dagshub.init(
+#         repo_owner=DAGSHUB_USERNAME,
+#         repo_name=DAGSHUB_REPO,
+#         mlflow=True
+#     )
+
+#     mlflow.set_experiment(experiment_name)
 
 
 # # production Environment MLFLOW Access
-# def setup_mlflow_connection():
-#     dagshub_token = os.getenv("MLFLOW_TOKEN")
-#     if not dagshub_token:
-#         raise EnvironmentError("Mlflow Token variable not set")
+def setup_mlflow_connection():
+    dagshub_token = os.getenv("MLFLOW_TOKEN")
+    if not dagshub_token:
+        raise EnvironmentError("Mlflow Token variable not set")
 
-#     os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-#     os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+    os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-#     dagshub_url = "https://dagshub.com"
-#     repo_owner = DAGSHUB_USERNAME
-#     repo_name= DAGSHUB_REPO
+    dagshub_url = "https://dagshub.com"
+    repo_owner = DAGSHUB_USERNAME
+    repo_name= DAGSHUB_REPO
 
-#     mlflow.set_tracking_uri(f"{dagshub_url}/{repo_owner}/{repo_name}.mlflow")
-#     # Set experiment
-#     mlflow.set_experiment(experiment_name)
+    mlflow.set_tracking_uri(f"{dagshub_url}/{repo_owner}/{repo_name}.mlflow")
+    # Set experiment
+    mlflow.set_experiment(experiment_name)
     
 
